5786e335da803acfd04743dde01e481d22e34ae8
unknown <phillipmay@videotron.ca>
2018-02-26 00:29:39 -0500

    A program that automatically calculates and fixes the checksum for a LoRom BSX game.

---
+++ win32_project2/Main not working backup.cpp
@@ -0,0 +1,92 @@
+﻿/*****************************************************************************
+*	FileName:		main.c
+*	
+*	Name: Lab1, BS-X checksum calculator
+*
+*	Description: BS-X checksum calculator
+*
+REVISION HISTORY
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Anthor			Date				Comments on this revision
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Phillip May		February 25th		First session
+*
+*	A windows command line tool to find the checksum of a BS-X rom
+*	The checksum is the 32bit integer sum of all the bits in the file ignoring the header.
+*	This number is then truncated to 16 bit and should be output in raw hex.
+*
+******************************************************************************/
+//Libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //For string copying
+#include <stdint.h>
+#include <io.h>
+#include <share.h>
+
+//Symbolic constants
+//GFX_SAFE keeps colour order as RED GREEN BLUE
+//GFX_AUTODETECT_WINDOWED changes colour order to BLUE GREEN RED
+#define		READONLY		"r"
+#define		ORIGIN			0
+#define		MAXREADSIZE		60000
+
+//Globals
+FILE *stm_ROM;
+
+int filedescriptor;	
+int num_position;
+int8_t str_buffer[MAXREADSIZE];
+unsigned int num_readbytes = MAXREADSIZE;
+int		bytesread;
+
+int32_t		hex_checksum32;
+
+//Main
+int main(void) {
+	//Main variables
+	printf("BS-X rom checksum calculator\n");
+
+	#define BANKSIZE 1 /* read 1024 bytes at a time */
+
+	hex_checksum32 = 0;
+	int8_t read_buffer[BANKSIZE];
+	FILE *fle_bsx;
+	size_t nread;
+	int64_t counter;
+	int		inp_temporary;
+
+	counter = 0;
+	inp_temporary = 0;
+
+	fle_bsx = fopen("rom.sfc", "r");
+	if (fle_bsx) 
+	{
+		//While not error
+    while ((nread = fread(read_buffer, 1, sizeof read_buffer, fle_bsx)) > 0)
+	{
+		//fwrite(read_buffer, 1, nread, stdout);
+		//Stop tpycasting from being stupid and filling new space with binary 1's
+		//I am not dealling with negatives or positives so that is unwated behavior.
+		inp_temporary = (read_buffer[0] & 0x000000FF);
+		hex_checksum32 = (hex_checksum32 + inp_temporary);
+		printf("\nCurrent 32bit checksum: 0x%04X",hex_checksum32);
+		printf("\nCurrent byte: 0x%02X",inp_temporary);
+
+	}
+
+    if (ferror(fle_bsx)) {
+        /* deal with error */
+    }
+    fclose(fle_bsx);
+}
+	printf("\nFinal 32bit checksum: 0x%04X",hex_checksum32);
+
+	//Visual studio debug buffer
+	getchar();
+	getchar();
+	getchar();
+
+}
+
+
---
+++ win32_project2/Main.cpp
@@ -0,0 +1,96 @@
+﻿/* fread example: read an entire file */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define FILENAME "BS1_patched.bs"
+
+int main () {
+  FILE * pFile;
+  long lSize;
+  int8_t * buffer;
+  size_t result;
+  uint32_t int_temporary;
+  uint32_t hex_32bitchecksum;
+  uint16_t hex_16bitchecksum;
+  uint16_t hex_16bittemporary;
+  //4 bytes being overwritten
+  uint8_t hex_streamwrite[4];
+
+  hex_32bitchecksum = 0;
+  int_temporary = 0;
+  hex_16bitchecksum = 0;
+
+  pFile = fopen ( FILENAME , "r+b" );
+  if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+
+  // obtain file size:
+  fseek (pFile , 0 , SEEK_END);
+  lSize = ftell (pFile);
+  rewind (pFile);
+
+  // allocate memory to contain the whole file:
+  buffer = (int8_t*) malloc (sizeof(char)*lSize);
+  if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+
+  // copy the file into the buffer:
+  result = fread (buffer,1,lSize,pFile);
+  if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
+
+  /* the whole file is now loaded in the memory buffer. */
+  	printf("File size: 0x%08X\n",lSize);
+
+	for (long i = 0; i < lSize; i++)
+	{
+		if (i < 0x07FB0 || i > 0x07FDF)
+		{
+			int_temporary = (buffer[i] & 0x000000FF);
+			hex_32bitchecksum = ( (0x000000FF & int_temporary) + hex_32bitchecksum);
+			//printf("Byte (int temporary) %d: 0x%08X\n",i,int_temporary);
+			//printf("Checksum (hex_32bitchecksum) %d: 0x%08X\n",i,hex_32bitchecksum);
+
+		}
+	}
+	printf("32bit checksum: 0x%08X\n",hex_32bitchecksum);
+	//Usefull outputs to insert into rom
+	hex_16bitchecksum = (hex_32bitchecksum & 0x0000FFFF);
+	hex_16bittemporary = hex_16bitchecksum << 8;
+	hex_16bitchecksum = hex_16bittemporary + (hex_16bitchecksum >> 8);
+
+
+	printf("16bit checksum: %4X\n",hex_16bitchecksum);
+	printf("16bit inverse checksum: %4X\n",~(hex_16bitchecksum));
+	printf("Size of 16bit int is: %d\n",sizeof(hex_16bitchecksum));
+	hex_streamwrite[0] = ~(hex_16bitchecksum) >> 8;
+	hex_streamwrite[1] = ~(hex_16bitchecksum);
+	hex_streamwrite[2] = hex_16bitchecksum >> 8;
+	hex_streamwrite[3] = hex_16bitchecksum;
+
+	printf("4 bytes going to file: %1X",hex_streamwrite[0]);
+	printf(" %1X",hex_streamwrite[1]);
+	printf(" %1X",hex_streamwrite[2]);
+	printf(" %1X\n",hex_streamwrite[3]);
+	printf("Size of array in bytes: %d\n",sizeof(hex_streamwrite));
+	printf("Size of array element bytes: %d\n",sizeof(int8_t));
+
+
+	//Now overwrite the bits and patch the rom
+	rewind (pFile);
+	fseek (pFile , (0x07FDC), SEEK_SET);
+	printf("Write to file error: %d",fwrite(hex_streamwrite,sizeof(int8_t),sizeof(hex_streamwrite),pFile));
+
+
+
+  // terminate
+  fclose (pFile);
+  free (buffer);
+
+  /*
+  //keep window open buffer
+  getchar();
+  getchar();
+  getchar();
+  */
+
+  return 0;
+}
